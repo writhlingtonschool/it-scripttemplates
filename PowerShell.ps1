a416a8c912c39e45ec5239abaa7949478d10bcb9
danielwiltshire <dwiltshire@writhlington.org.uk>
2018-04-29 12:54:10 +0100

    added header comment template

--- PowerShell.ps1
+++ PowerShell.ps1
@@ -1,4 +1,12 @@
-$LogFile = "%BUNDLE_LOG%"
+# This script does x, y and z.
+
+# Step 1: Do X
+# Step 2: Do Y
+# Step 3: Do Z
+
+# Specify the location of the log file
+# For ZENworks, this might be %BUNDLE_LOG_WIN% or %BUNDLE_LOG_MAC%
+$LogFile = "C:\path\to\log.log"
 
 #Function that logs a message to a text file
 function LogMessage
